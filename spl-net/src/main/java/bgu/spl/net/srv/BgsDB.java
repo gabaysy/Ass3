be073cc3768ad1bfc103775125d9676d434ca2b9
gabaysy <gabaysy@gmail.com>
2021-12-27 22:01:12 +0200

    user DB.

--- spl-net/src/main/java/bgu/spl/net/srv/BgsDB.java
+++ spl-net/src/main/java/bgu/spl/net/srv/BgsDB.java
@@ -6,24 +6,29 @@ import java.util.List;
 import java.util.Map;
 
 public class BgsDB {
-    private Map<Integer, User> users;
+    private Map<String, User> users;
     private int nextId;
 
     public BgsDB (){
         this.users=new HashMap();
+        this.nextId=1;
     }
 
 
 
 public boolean register (String name, String code, String date){
-        if(containName(name))
+//        if(containName(name))
+    if(users.containsKey(name))
             return false;
-        users.add(new User(name , code , date));
+        users.putIfAbsent(name,new User(name , code , date));
         return true;
 }
 
     public boolean logIn(String name, String code){
-        if (containName(name) ||(! code.equals() ))
+        if (containName(name) ||(! code.equals(users.get(name).getPassword()) ) ||users.get(name).isloggedin())  // not register/Password doest match/ already logging
+            return false;
+        users.get(name).login();
+        return true;
     }
 
 
