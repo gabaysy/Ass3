acf15d6500910c1d6a93c97a9c54223b777fcceb
gabaysy <gabaysy@gmail.com>
2021-12-29 12:48:56 +0200

    lala

--- spl-net/src/main/java/bgu/spl/net/srv/BGS/BidiMessagingProtocolImpl.java
+++ spl-net/src/main/java/bgu/spl/net/srv/BGS/BidiMessagingProtocolImpl.java
@@ -5,7 +5,7 @@ import bgu.spl.net.api.bidi.Connections;
 import bgu.spl.net.srv.BGS.msg.Message;
 import bgu.spl.net.srv.BgsDB;
 
-public class BidiMessagingProtocolImpl implements BidiMessagingProtocol {
+public class BidiMessagingProtocolImpl<T> implements BidiMessagingProtocol {
 
 
     private boolean shouldTerminate = false;
---
+++ spl-net/src/main/java/bgu/spl/net/srv/ServerTPC.java
@@ -0,0 +1,10 @@
+package bgu.spl.net.srv;
+
+public class ServerTPC extends BaseServer {
+
+
+    @Override
+    protected void execute(BlockingConnectionHandler handler) {
+        new Thread()
+    }
+}
