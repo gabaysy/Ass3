86bdf3c1425eebba23e560ec33af9c53d60ad7dc
YaelMagrafta <yael.magrafta@gmail.com>
2022-01-05 17:27:01 +0200

    TCP

--- spl-net/src/main/java/bgu/spl/net/srv/TPCMain.java
+++ spl-net/src/main/java/bgu/spl/net/srv/TPCMain.java
@@ -9,13 +9,13 @@ public class TPCMain {
     public static void main(String[] args) {
 
         BgsDB DB = new BgsDB(); //one shared object
-
-        Server.threadPerClient(
+        ServerTPC server= new ServerTPC(
             //    Integer.parseInt(args[0]), //port
                 7777,
                 () -> new BidiMessagingProtocolImpl(DB), //protocol factory
                 MessageEncoderDecoder::new //message encoder decoder factory
-        ).serve();
+        );
+        server.serve();
 
     }
 }
